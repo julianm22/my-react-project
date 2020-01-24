e4ae4929eec13b9816e9f20a32fbf5ab6bb4bb71
Julian-Murillo <jomsk822@gmail.com>
2020-01-24 14:04:17 -0500

    added referenceto input element in class based Person component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,16 @@ import withClass from '../../../hoc/withClass';
 // import styled from 'styled-components';
 
 class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
@@ -15,7 +25,10 @@ class Person extends Component {
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
                 <p>{this.props.children}</p>
-                <input type="text" 
+                <input 
+                    // ref={(inputEl) => {this.inputElement = inputEl}}
+                    ref={this.inputElementRef}
+                    type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} 
                 />
