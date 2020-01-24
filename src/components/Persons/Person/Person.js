3628e84a4d6722c6902e49e02bc1e5e759adf8cc
Julian-Murillo <jomsk822@gmail.com>
2020-01-24 15:16:49 -0500

    switched to using contextType in class based Person component and useContext hook in functional Cockpit component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -13,18 +13,19 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         // this.inputElement.focus();
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated);
     }
 
     render() {
         console.log('[Person.js] rendering...');
         return (
             <Auxiliary>
-                <AuthContext.Consumer>
-                    {(context) => context.authenticated ? <p>Authenticated!</p> : <p>Please login</p>}
-                </AuthContext.Consumer>
+                {this.context.authenticated ? <p>Authenticated!</p> : <p>Please login</p>}
                 <p onClick={this.props.click}>
                     I'm {this.props.name} and I am {this.props.age} years old!
                 </p>
@@ -41,7 +42,7 @@ class Person extends Component {
     }
 }
 
-Person.PropTypes = {
+Person.propTypes = {
     click: PropTypes.func,
     name: PropTypes.string,
     age: PropTypes.number,
